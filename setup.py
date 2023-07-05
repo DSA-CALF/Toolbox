2542cffd87ce0e67bbb841f09702fee0fe5ca276
Zacharie BUISSON <BUISSOZA@harmonie.com>
2023-07-05 17:34:59 +0200

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="ToolboxDSA",
+    version="0.0.1",
+    author="DSA_CALF",
+    author_email="calfdsa@gmail.com",
+    description="Tool Box of different relevant analysis, statistical tests and modelization tools used by CALF.",
+    url="https://github.com/DSA-CALF/Toolbox",
+    license="MIT", 
+    packages=["ToolboxDSA"]
+)
