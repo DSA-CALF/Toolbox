ff9cec7a83b556c1554bda5e50fb3e4d7d482282
Zacharie BUISSON <BUISSOZA@harmonie.com>
2023-07-05 17:42:12 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     author="DSA_CALF",
     author_email="calfdsa@gmail.com",
     description="Tool Box of different relevant analysis, statistical tests and modelization tools used by CALF.",
+    long_description="Tool Box of different relevant analysis, statistical tests and modelization tools used by CALF.",
     url="https://github.com/DSA-CALF/Toolbox",
     license="MIT", 
     packages=["ToolboxDSA"]
